392c68ddc5e458bb8a61b2a0f17947899e1c7a58
yi.yan11 <yi.yan11@600900B211DE405>
2018-03-07 15:56:32 +1100

    Added Astroides

---
+++ Assets/~Asteroids/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+namespace Asteroids
+{
+    public class PlayerController : MonoBehaviour
+
+    {
+        public Moving movement;
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            float inputV = Input.GetAxis("Vertical");
+            float inputH = Input.GetAxis("Horizontal");
+            // -1 == A || LeftArrow
+            // 0 == Not being pressed
+            // 1 == D || RightArrow
+            if (inputV > 0)
+            {
+                movement.Accelerate(transform.up);
+            }
+            
+        }
+    }
+}
