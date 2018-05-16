316a036f5dc47ab1dd6be45a63ba3c8bb5931931
yi.yan11 <yi.yan11@600900B211DE405>
2018-05-16 15:59:03 +1000

    100 Ball drop

---
+++ Unity/Assets/~AHundredBalls/Scripts/PaddleScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour
+{
+
+    private Animator anim;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            anim.SetBool("isOpened", true);
+        }
+
+        else
+        {
+            anim.SetBool("isOpened", false);
+        }
+
+    }
+}
---
+++ Unity/Assets/~AHundredBalls/Scripts/SpawnerScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+
+    public GameObject[] prefabs = null;
+    public float spawnRadius = 5.0f;
+    public float spawnRate = 1.0f;
+    private float spawnFactor = 0.0f;
+
+    void Update()
+    {
+        HandleSpawn();
+
+    }
+
+    void HandleSpawn()
+    {
+        spawnFactor += Time.deltaTime;
+
+        if(spawnFactor > spawnRate)
+        {
+            int randomIndex = Random.Range(0, prefabs.Length);
+            Spawn(prefabs[randomIndex]);
+            spawnFactor = 0;
+        }
+    }
+
+    void Spawn(GameObject _object)
+    {
+        GameObject newObject = Instantiate(_object);
+        Vector3 randomPoint = Random.insideUnitCircle * spawnRadius;
+        newObject.transform.position = transform.position + randomPoint;
+    }
+}
