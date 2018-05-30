dc6b25ac6a74790aed3bf7426aaa5ed1913c2235
Unknown <yirulz@hotmail.com>
2018-05-30 15:46:18 +1000

    Buckets

---
+++ Unity/Assets/~AHundredBalls/Scripts/BucketScoring.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BucketScoring : MonoBehaviour
+{
+    public int score;
+    public Text scoreText;
+
+    void Start()
+    {
+        scoreText.text = "";
+    }
+
+    void Update()
+    {
+        scoreText.text = "Your score is " + score;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(col.gameObject);
+        score++;
+    }
+   
+}
